423ff695fa5b2d581c4954a8455fbaf5a9be4f62
DongJoonLeeDJ <ldjgood12@gmail.com>
2024-03-19 10:14:30 +0900

    Hello World!!!

---
+++ djCSharp/02_begin_c#/HelloCSharp001/HelloCSharp001/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloCSharp001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 주석, 주석은 타자에 자신 없으면 따라 적지 마세요.
+            //타자 자체에 자신이 없으면 그냥 코드를 보셔도 됩니다.
+            // 동영상 녹화, github 공유, 공유폴더에 공유하기 때문이죠.
+
+            //cw 적고 tab키 두 번 누르기
+            //Console.WriteLine = System.out.println
+            //Console.Write = System.out.print
+            Console.WriteLine("안녕"); //실행하는 건 ctrl f5
+            Console.Write("Hello "); 
+            Console.Write("World!"); //ctrl d 누르면 한 줄 복붙
+            Console.Write("World!");
+            Console.WriteLine(); //그냥 한 줄 띄기만 수행
+            //자바에서 sysout 적으면 System.out.println 생기듯이
+            //c#에서는 cw 적고 tab키 두 번 누르면 Console.WriteLine이 생김
+            //이러한 cw 등을 '코드 조각'이라고 부르고 사용자가 직접
+            //새로운 코드 조각을 만들 수도 있다.
+            
+        }
+    }
+}
