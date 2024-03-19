b8fb6ecf1fc6db0a14dc7088b3e7fddb28e5f742
DongJoonLeeDJ <ldjgood12@gmail.com>
2024-03-19 14:45:46 +0900

    별찍기

--- djCSharp/02_begin_c#/HelloCSharp001/HelloCSharp001_5/Program.cs
+++ djCSharp/02_begin_c#/HelloCSharp001/HelloCSharp001_5/Program.cs
@@ -11,7 +11,6 @@ namespace HelloCSharp001_5
         static void Main(string[] args)
         {
             StartPos:
-
             int sum = 0;
             for (int i = 1; i <= 100; i++)
                 sum += i;
---
+++ djCSharp/02_begin_c#/HelloCSharp001/HelloCSharp001_6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloCSharp001_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int floor = 5; 
+            
+            for(int i = 0; i < floor; i++) 
+            {
+                //공백 찍는 부분
+                for (int j = floor - i; j > 1; j--)
+                    Console.Write(" ");
+
+                //별 찍는 부분
+                for (int j = 0; j < 2 * i + 1; j++)
+                    Console.Write("*");
+                Console.WriteLine();//줄 띄어쓰기
+            }
+        }
+    }
+}
