bde4a1e943272596491ef2fade1e4596de928383
DongJoonLeeDJ <ldjgood12@gmail.com>
2024-03-19 15:08:51 +0900

    조건문 반복문 마무리 및 디버깅

--- djCSharp/02_begin_c#/HelloCSharp001/HelloCSharp001_6/Program.cs
+++ djCSharp/02_begin_c#/HelloCSharp001/HelloCSharp001_6/Program.cs
@@ -23,6 +23,31 @@ namespace HelloCSharp001_6
                     Console.Write("*");
                 Console.WriteLine();//줄 띄어쓰기
             }
+
+            int[] numbers = new int[5];
+            for(int i = 0; i<numbers.Length; i++) 
+            {
+                Console.WriteLine(i+1+"번째 값 입력");
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+            //가장 첫번째 값이 최댓값, 최솟값이라고 가정
+            int max = numbers[0];
+            int min = numbers[0];
+            for(int i = 1; i<numbers.Length; i++)
+            {
+                if (max < numbers[i])
+                    max = numbers[i];
+                if (min > numbers[i]) //if if 대신 //if else를 쓰게 되면 안 됨
+                    min = numbers[i]; //다만 if else if를 쓸 경우에는 되긴 함
+            }
+            Console.WriteLine("최댓값:"+max);
+            Console.WriteLine("최솟값:"+min);
+
+            //한글 다 출력하기
+            for (char i = '가'; i <= '힣'; i++)
+                Console.Write(i);
+            Console.WriteLine();
+
         }
     }
 }
