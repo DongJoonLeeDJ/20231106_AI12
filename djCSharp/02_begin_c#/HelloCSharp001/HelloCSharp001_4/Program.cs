a830c235dded522869dbe6ea4ee2c95edf47bd77
DongJoonLeeDJ <ldjgood12@gmail.com>
2024-03-19 12:40:32 +0900

    Update Program.cs

--- djCSharp/02_begin_c#/HelloCSharp001/HelloCSharp001_4/Program.cs
+++ djCSharp/02_begin_c#/HelloCSharp001/HelloCSharp001_4/Program.cs
@@ -82,7 +82,49 @@ namespace HelloCSharp001_4
                     Console.WriteLine("용띠");
                     break;
                 default:
-                    Console.WriteLine($"{year}???");
+                    Console.WriteLine("이게 나올 일은 없다.");
+                    //Console.WriteLine($"{year}???");
+                    break;
+            }
+
+            Console.WriteLine("몇 월에 태어나셨나요?");
+            int month = int.Parse(Console.ReadLine());
+            if(month==12 || month == 1 || month==2)
+                Console.WriteLine("겨울");
+            else if(month>=3 && month <=5)
+                Console.WriteLine("봄");
+            else if(month>5 && month <9) //6,7,8
+                Console.WriteLine("여름");
+            else if(month>=9 && month<=11)
+                Console.WriteLine("가을");
+            else
+                Console.WriteLine("어느 별에서 왔니?");
+
+            month = DateTime.Now.Month;
+            switch (month)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("Winter");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("Spring");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("Summer");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("Autumn");
+                    break;
+                default:
+                    Console.WriteLine("?????????");
                     break;
             }
 
