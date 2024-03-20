65568503081114087ff0abe5ab1a8be6904a5a22
DongJoonLeeDJ <ldjgood12@gmail.com>
2024-03-20 14:05:51 +0900

    5단원끝

---
+++ djCSharp/03_class_c#/HelloCSharp02/HelloCSharp02/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloCSharp02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //프로그램 시작하자마자 label1과 label2에 있는 텍스트를 지움
+            label1.Text = "";
+            label2.Text = ""; 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = textBox_test.Text;
+            int[] lotto = new int[6];
+            Random r = new Random();
+            for(int i = 0; i<lotto.Length; i++)
+            {
+                int num = r.Next(45)+1; //1~45까지의 값
+                if(lotto.Contains(num)) //num이 lotto안에 이미 있는 경우
+                {
+                    i--;
+                    continue;
+                }
+                lotto[i] = num;
+            }
+            int bns = r.Next(45) + 1;
+            while(lotto.Contains(bns))
+                bns = r.Next(45) + 1;
+            Array.Sort(lotto); //오름 차순 정렬
+            label2.Text = string.Join(",", lotto);
+            label2.Text += " 보너스 번호 : " + bns;
+        }
+
+        //버튼 클릭시 디자인 창에서는 없었던
+        //텍스트 박스 하나가 새로 생기게 됨
+        private void button2_Click(object sender, EventArgs e)
+        {
+            TextBox temp = new TextBox();
+            temp.Text = "임시 텍스트 박스";
+            temp.Location = new Point(100, 100);
+            Controls.Add(temp);
+        }
+    }
+}
---
+++ djCSharp/03_class_c#/HelloCSharp02/HelloCSharp022/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloCSharp022
+{
+    internal class Program
+    {
+        //static이 붙어 있는 메서드 안에는 static이 붙은
+        //메서드나 변수만 사용 가능함
+        //static 없는 애는 쓸 수 없음.
+        static void HelloWorld()
+        {
+            Console.WriteLine("Hello World!");
+        }
+        void HiWorld()
+        {
+            Console.WriteLine("Hi World!");
+        }
+        static int count = 0;
+        int number = 0;
+        static void Main(string[] args)
+        {
+            HelloWorld();
+            //HiWorld();//static 붙은 애가 메모리가 먼저 올라감
+            //static 시점에선 static 붙지 않은 변수나 함수는
+            //아직 존재하지 않는 것이다.
+            Console.WriteLine(count);
+            //Console.WriteLine(number);
+
+            //굳이 static 없는 걸 Main에서 쓰고 싶다면
+            //자기 자신을 객체화 해야 함
+            //이렇게 하면 메모리가 할당이 되게 되고
+            //그러면 static이 붙은 메서드 안에서도 쓸 수 있음
+            new Program().HiWorld();
+
+            //인스턴스 별로 다른 값 갖는 것 : 인스턴스 변수(static 없음)
+            //클래스 별로 같은 값 갖는 것 : 클래스 변수(static 있음)
+            Product p1 = new Product();
+            p1.name = "감자";
+            p1.price = 1000;
+            Product.countOfProduct++;
+
+            Product p2 = new Product() {name="당근",price=500};
+            Product.countOfProduct++;
+
+            Console.WriteLine(p1.name);
+            Console.WriteLine(p1.price);            
+            Console.WriteLine(p2.name);
+            Console.WriteLine(p2.price);
+            Console.WriteLine(Product.countOfProduct);
+
+
+
+        }
+    }
+}
