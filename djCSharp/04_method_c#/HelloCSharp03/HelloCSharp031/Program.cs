0470771d56bdb559b06ba82e3a09c4c6ba3b7d21
DongJoonLeeDJ <ldjgood12@gmail.com>
2024-03-21 10:43:02 +0900

    swap_구조체_재귀함수_Dictionary

---
+++ djCSharp/04_method_c#/HelloCSharp03/HelloCSharp031/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloCSharp031
+{
+    internal class Program
+    {
+        static void swap(int a, int b)//main의 원본에
+        {                                   //영향을 주지 않음
+            int t = a; 
+            a = b;
+            b = t;
+        }
+        static void swap(ref int a, ref int b)//원본도 같이 바뀜
+        {                      
+            int t = a;
+            a = b;
+            b = t;
+        }
+        static void change(Student student)
+        {
+            student.name = "안위재";
+            student.age = 40;
+            student.score = 75;
+        }
+        static void Main(string[] args)
+        {
+            int a = 3;
+            int b = 5;
+            int t = a;
+            Console.WriteLine("a="+a+",b="+b);
+            a = b;
+            b = t;
+            Console.WriteLine("a="+a+",b="+b);
+            swap(a, b);
+            Console.WriteLine("a="+a+",b="+b);
+            swap(ref a, ref b);
+            Console.WriteLine("a="+a+",b="+b);
+
+            Student s = new Student();
+            s.age = 30;
+            s.name = "권성직";
+            s.score = 100;
+            Student dj = s;
+            dj.score = 0;
+            dj.name = "이동준";
+            Console.WriteLine(s.name+","+s.age+","+s.score);
+            Console.WriteLine(dj.name+","+ dj.age+","+ dj.score);
+            change(dj);
+            Console.WriteLine(dj.name+","+ dj.age+","+ dj.score);
+        }
+    }
+}
---
+++ djCSharp/04_method_c#/HelloCSharp03/HelloCSharp031/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloCSharp031
+{
+    public struct Student //구조체 != 클래스
+    {//복사하는 방식이 값 복사 방식을 쓴다.
+        public string name { get; set; }
+        public int age { get; set; }
+        public int score { get; set; }
+    }
+}
