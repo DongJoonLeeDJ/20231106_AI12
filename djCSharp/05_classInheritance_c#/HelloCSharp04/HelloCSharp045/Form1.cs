cc4fe609482645e2a68df897409573edbd99082e
DongJoonLeeDJ <ldjgood12@gmail.com>
2024-03-25 16:24:15 +0900

    datagridview 즉 10단원까지 함

---
+++ djCSharp/05_classInheritance_c#/HelloCSharp04/HelloCSharp045/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloCSharp045
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView3.Columns[0].HeaderText = "학번";
+            dataGridView3.Columns[1].HeaderText = "이름";
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Add(textBox1.Text, textBox2.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dataGridView2.Columns.Add(textBox3.Text, textBox4.Text);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                dataGridView2.Rows.Add(textBox4.Text);
+                dataGridView2.Rows.Add(textBox1.Text, textBox4.Text);
+            }
+            catch (Exception)
+            {
+
+                //throw;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            studentBindingSource.Add
+                (new Student() { name = textBox6.Text, hakbeon = textBox7.Text });
+        }
+
+        List<Student> students = new List<Student>();
+        private void button5_Click(object sender, EventArgs e)
+        {
+            students.Add(new Student() { name = textBox8.Text, hakbeon = textBox9.Text });
+            dataGridView4.DataSource = null;
+            dataGridView4.DataSource = students;
+        }
+    }
+}
---
+++ djCSharp/05_classInheritance_c#/HelloCSharp04/HelloCSharp045/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloCSharp045
+{
+
+    //datagridview 바인딩 하기 전 반드시 해야할 것 (둘 중 하나)
+    //1. 한 번 실행을 하고 나서 바인딩한다.
+    //2. 빌드->솔루션 정리, 솔루션 빌드, 솔루션 다시 빌드 누르고 바인딩한다.
+    public class Student
+    {
+        public string hakbeon { get; set; } //이거 2개만 데이터그리드뷰에 나옴
+        public string name { get; set; } //이거 2개만 데이터그리드뷰에 나옴
+
+        public int age; //이건 datagridview에 안 나옴!!!!
+    }
+}
