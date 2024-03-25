e533cb542b0c8add0a425316ef183b571773afe3
DongJoonLeeDJ <ldjgood12@gmail.com>
2024-03-25 11:22:58 +0900

    간단 실습 문제 (창 색깔)

---
+++ djCSharp/05_classInheritance_c#/HelloCSharp04/HelloCSharp041/Form2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace HelloCSharp041
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        public Form2(string txt)
+        {
+            InitializeComponent();
+
+            if (txt.Equals("red"))
+            {
+                BackColor = Color.Red;
+            }
+            else if (txt.Equals("blue"))
+            {
+                BackColor = Color.Blue;
+            }
+        }
+    }
+}
