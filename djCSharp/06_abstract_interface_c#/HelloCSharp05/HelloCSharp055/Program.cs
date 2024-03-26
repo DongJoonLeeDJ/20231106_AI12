e4e1999ec42a234625236723655b4298eef67633
DongJoonLeeDJ <ldjgood12@gmail.com>
2024-03-26 15:00:26 +0900

    linq xml

--- djCSharp/06_abstract_interface_c#/HelloCSharp05/HelloCSharp055/Program.cs
+++ djCSharp/06_abstract_interface_c#/HelloCSharp05/HelloCSharp055/Program.cs
@@ -10,18 +10,18 @@ namespace HelloCSharp055
     {
         static void Main(string[] args)
         {
-            List<int> nums = new List<int>() {1,2,3,4,5 };
+            List<int> nums = new List<int>() { 1, 2, 3, 4, 5 };
 
             //만약 짝수만 가져오고 싶다면?
             List<int> evenNum = new List<int>();
-            foreach(var item in nums)
+            foreach (var item in nums)
             {
-                if(item%2==0)
+                if (item % 2 == 0)
                     evenNum.Add(item);
             }
             //LINQ = 컬렉션(=배열이나 List)에 있는 값을 SQL 형식으로 가져오는 문법
             // from 요소이름(=임의변수이름) in 컬렉션명 where 조건 select 가져올 값;
-            var eventNum2 = from item in nums where item %2==0 select item;
+            var eventNum2 = from a in nums where a % 2 == 0 select a;
 
             foreach (var item in evenNum)
                 Console.WriteLine(item);
@@ -31,10 +31,10 @@ namespace HelloCSharp055
             List<int> powerNum = new List<int>();
             foreach (var item in nums)
                 powerNum.Add(item * item);
-            foreach(var item in powerNum)
+            foreach (var item in powerNum)
                 Console.WriteLine(item);
 
-            var powerNum2 = from item in nums select item * item;
+            var powerNum2 = from aa in nums select aa * aa;
             foreach (var item in powerNum2)
                 Console.WriteLine(item);
 
@@ -42,10 +42,51 @@ namespace HelloCSharp055
             List<int> mypower = powerNum2.ToList();
 
             List<int> oddNum = (from item in nums where item % 2 != 0 select item).ToList<int>();
-            foreach(var item in oddNum)
+            foreach (var item in oddNum)
                 Console.WriteLine(item);
 
+            //익명 객체
+            var powerEvenOutput = from item in nums
+                                  where item % 2 == 0
+                                  select new
+                                  {
+                                      num = item,
+                                      doubleNum = item * 2,
+                                      powerNum = item * item
+                                    };
+            foreach(var item in powerEvenOutput)
+            {
+                Console.WriteLine(item.num+","+item.doubleNum+","+item.powerNum);
+            }
 
+            
+            
+            List<Product> products = new List<Product>();
+            products.Add(new Product() { name = "고구마", price = 5000 });
+            products.Add(new Product() { name = "마", price = 2500 });
+            products.Add(new Product() { name = "당근", price = 9000 });
+            products.Add(new Product() { name = "파", price = 500 });
+            products.Add(new Product() { name = "감자", price = 5500 });
+
+            //LINQ의 장단점
+            //장점 : for와 if없이 쉽게 값을 뽑아낼 수 있다.
+            //단점 : 이 코드들이 모두 1줄로 간주되기 때문에 오류가 생기면 디버깅하기가 너무 힘들다.
+            var myproducts = from item in products
+                             where item.price > 5000
+                             orderby item.name ascending
+                             select item;
+            List<Product> vipProducts = (from item in products
+                                        where item.price > 5000
+                                        orderby item.name ascending
+                                        select item).ToList<Product>();
+            foreach(var item in myproducts)
+                Console.WriteLine(item.ToString());
+            foreach(var item in vipProducts)
+                Console.WriteLine(item.ToString());
+            foreach(Product item in myproducts)
+                Console.WriteLine(item.ToString());
+            foreach (Product item in vipProducts)
+                Console.WriteLine(item.ToString());
 
 
         }
