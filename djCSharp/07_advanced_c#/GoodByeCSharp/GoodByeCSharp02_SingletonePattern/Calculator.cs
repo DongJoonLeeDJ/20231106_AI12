6f9dde4d1bbbd9ae281184e9b569e7434629aa4f
DongJoonLeeDJ <ldjgood12@gmail.com>
2024-03-28 12:37:08 +0900

    mvc 설명 및 언급하고 싱글톤 같이 해 봄

---
+++ djCSharp/07_advanced_c#/GoodByeCSharp/GoodByeCSharp02_SingletonePattern/Calculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodByeCSharp02_SingletonePattern
+{
+    public class Calculator
+    {
+        public int plus(int x, int y) { return x + y; }
+        public virtual int minus(int x, int y) { return x - y; }
+    }
+}
---
+++ djCSharp/07_advanced_c#/GoodByeCSharp/GoodByeCSharp02_SingletonePattern/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodByeCSharp02_SingletonePattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int test = MyMath.getInstance().power(5);
+            //int test = MyMath.power(5);
+            Console.WriteLine(  test  );
+            Console.WriteLine( MyMath._getInstance.power(5) );
+        }
+    }
+}
