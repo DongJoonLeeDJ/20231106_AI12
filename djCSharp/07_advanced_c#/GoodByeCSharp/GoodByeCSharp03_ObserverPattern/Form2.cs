47b46f5f36afd360a244a17476241b533821afbe
DongJoonLeeDJ <ldjgood12@gmail.com>
2024-03-28 14:27:41 +0900

    Update Form2.cs

--- djCSharp/07_advanced_c#/GoodByeCSharp/GoodByeCSharp03_ObserverPattern/Form2.cs
+++ djCSharp/07_advanced_c#/GoodByeCSharp/GoodByeCSharp03_ObserverPattern/Form2.cs
@@ -10,11 +10,22 @@ using System.Windows.Forms;
 
 namespace GoodByeCSharp03_ObserverPattern
 {
-    public partial class Form2 : Form
+    public partial class Form2 : Form, IObserver
     {
         public Form2()
         {
             InitializeComponent();
+        }
+        //ISubject를 구현한 객체가 가지고 있는 register를 호출함
+        public Form2(ISubject sub)//현재는 ISubject를 구현한 객체 = Form1
+        {
+            InitializeComponent();
+            sub.register(this); //Form1에 있는 register를 호출함
+        }
+
+        public void update(string value)
+        {
+            label1.Text = "옵저버 패턴에 의한 변경 메시지 : " + value;
         }
     }
 }
