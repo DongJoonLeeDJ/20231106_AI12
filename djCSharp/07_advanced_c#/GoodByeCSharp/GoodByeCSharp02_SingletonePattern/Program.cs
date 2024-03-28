44156dd44701fae14505e39e34c6b63820d00ae2
DongJoonLeeDJ <ldjgood12@gmail.com>
2024-03-28 13:11:27 +0900

    싱글톤 추가 예시

--- djCSharp/07_advanced_c#/GoodByeCSharp/GoodByeCSharp02_SingletonePattern/Program.cs
+++ djCSharp/07_advanced_c#/GoodByeCSharp/GoodByeCSharp02_SingletonePattern/Program.cs
@@ -8,12 +8,21 @@ namespace GoodByeCSharp02_SingletonePattern
 {
     internal class Program
     {
+        static MyMath m = MyMath._getInstance;
         static void Main(string[] args)
         {
+            MyMath m2 = MyMath._getInstance;
             int test = MyMath.getInstance().power(5);
             //int test = MyMath.power(5);
             Console.WriteLine(  test  );
             Console.WriteLine( MyMath._getInstance.power(5) );
+
+            Console.WriteLine(  MyMath._getInstance.plus(5,3)  );
+            Console.WriteLine(  MyMath._getInstance.minus(5,3)  );
+            Console.WriteLine(  MyMath._getInstance.minus(3,5)  );
+
+            Console.WriteLine( m.power(100)) ;
+            Console.WriteLine( m2.power(500));
         }
     }
 }
