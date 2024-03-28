81b37003d2c463ec68478ff8afa92eb3a28f908a
DongJoonLeeDJ <ldjgood12@gmail.com>
2024-03-28 10:19:26 +0900

    Update Form3.cs

--- djCSharp/07_advanced_c#/GoodByeCSharp/GoodByeCSharp01_BookManager/Form3.cs
+++ djCSharp/07_advanced_c#/GoodByeCSharp/GoodByeCSharp01_BookManager/Form3.cs
@@ -12,9 +12,42 @@ namespace GoodByeCSharp01_BookManager
 {
     public partial class Form3 : Form
     {
+
+        bool FindBook(Book x)
+        {
+            return x.userId == textBox1.Text;
+        }
         public Form3()
         {
             InitializeComponent();
+            dataGridView1.DataSource = null;
+            if (DataManager.Users.Count > 0)
+                dataGridView1.DataSource = DataManager.Users;
+
+            dataGridView1.CellClick += DataGridView1_CellClick;
+
+            //추가
+            button1.Click += (s, e) =>
+            {
+
+            };
+            //수정
+            button2.Click += delegate (object s, EventArgs e)
+            {
+
+            };
+            //삭제
+            button3.Click += deleteUser;
+        }
+
+        private void DataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void deleteUser(object s, EventArgs e)
+        {
+
         }
     }
 }
