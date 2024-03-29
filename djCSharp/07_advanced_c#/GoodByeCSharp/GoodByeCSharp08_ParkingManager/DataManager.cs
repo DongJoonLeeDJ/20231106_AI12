3f70d37ac67c19717d3927d372c0a43db7dd0ffd
DongJoonLeeDJ <ldjgood12@gmail.com>
2024-03-29 12:03:16 +0900

    Update DataManager.cs

--- djCSharp/07_advanced_c#/GoodByeCSharp/GoodByeCSharp08_ParkingManager/DataManager.cs
+++ djCSharp/07_advanced_c#/GoodByeCSharp/GoodByeCSharp08_ParkingManager/DataManager.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace GoodByeCSharp08_ParkingManager
 {
-    internal class DataManager
+    public class DataManager
     {
+        public static List<ParkingCar> Cars = new List<ParkingCar>();
+        private static DBHelper_MSSQL mssql = DBHelper_MSSQL.getIntance; //싱글톤
+
+        public static void Load() //전체 불러오기
+        {
+
+        }
+        //contents = 로그(=기록)남길 때 쓸 것임
+        public static void Save(string cmd, string ps, out string contents) //주차 공간 추가 삭제용 Save
+        {
+
+        }
+        public static void Save() //주차 출차용
+        {
+
+        }
     }
 }
