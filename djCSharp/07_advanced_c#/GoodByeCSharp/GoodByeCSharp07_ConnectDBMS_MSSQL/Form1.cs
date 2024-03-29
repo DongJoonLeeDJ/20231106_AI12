e3c3fc8ef9121840227561526201d5c345c559b5
DongJoonLeeDJ <ldjgood12@gmail.com>
2024-03-29 10:47:21 +0900

    Update Form1.cs

--- djCSharp/07_advanced_c#/GoodByeCSharp/GoodByeCSharp07_ConnectDBMS_MSSQL/Form1.cs
+++ djCSharp/07_advanced_c#/GoodByeCSharp/GoodByeCSharp07_ConnectDBMS_MSSQL/Form1.cs
@@ -109,7 +109,27 @@ namespace GoodByeCSharp07_ConnectDBMS_MSSQL
         private void button2_Click(object sender, EventArgs e)
         {
             sendQuery("insert into testtable values(@hakbeon,@name)", CRUD.INSERT);
-            button1.PerformClick(); //실제 버튼 클릭한 것
+
+            try
+            {
+                ConnectDB(); //DBMS에 연결한 것
+                SqlCommand cmd = new SqlCommand(); //SQL문 보낼 객체
+                cmd.Connection = conn; //어디로 연결할 지 지정
+                cmd.CommandText = "select * from testtable"; //전송할 SQL문 작성
+                SqlDataAdapter da = new SqlDataAdapter(cmd); //SQL 데이터 처리할 변수
+                DataSet ds = new DataSet(); //실제 데이터 저장할 객체
+                da.Fill(ds, "mytest"); //da를 통해서 ds에 mytest라는 이름으로 sql 결과문을 넣음
+                dataGridView1.DataSource = null;
+                dataGridView1.DataSource = ds;
+                dataGridView1.DataMember = "mytest";
+            }
+            catch (Exception ex)
+            {
+            }
+            finally
+            {
+                conn.Close(); //java에서는 이 코드를 try catch 감싸야 하기도 함
+            }
         }
 
         private void button3_Click(object sender, EventArgs e)
