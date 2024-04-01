85a0ed0ece80a4fb5b9821bc6e946e7f37630ecf
DongJoonLeeDJ <ldjgood12@gmail.com>
2024-04-01 12:34:17 +0900

    지도 api 완성

--- djCSharp/07_advanced_c#/GoodByeCSharp/GoodByeCSharp10_mapAPI/KakaoAPI.cs
+++ djCSharp/07_advanced_c#/GoodByeCSharp/GoodByeCSharp10_mapAPI/KakaoAPI.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace GoodByeCSharp10_mapAPI
 {
-    internal class KakaoAPI
+    public class KakaoAPI
     {
+        public static List<Locale> Search(string text)
+        {
+            List<Locale> list = new List<Locale>();
+
+            //로컬 정보를 가져올 것임(이름과 좌표정보)
+            //카카오 로컬 API 활용 예정(JSON)
+
+
+
+
+            return list;
+        }
     }
 }
