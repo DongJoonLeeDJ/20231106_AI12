ed82a77223c4ec986b217d249bdcc40464921eff
DongJoonLeeDJ <ldjgood12@gmail.com>
2024-04-12 16:03:18 +0900

    Create Student.py

---
+++ djpython/pythonProject/Student.py
@@ -0,0 +1,7 @@
+class Student: #self 키워드는 무조건 필수
+    def __init__(self):
+        pass # 아무 것도 안 한다는 의미
+    def doStudent(self):
+        print('몇 시간 공부할래?')
+        studytime = int(input())
+        print('%d시간 공부합니다.' % studytime)
