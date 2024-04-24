69eb5eb2cd605ba73c7b95d44f1d7d616b8164b2
DongJoonLeeDJ <ldjgood12@gmail.com>
2024-04-24 15:04:28 +0900

    Update 05_create_model.py

--- djpython/studychatbot/05_create_model.py
+++ djpython/studychatbot/05_create_model.py
@@ -15,6 +15,9 @@ komoran = Komoran()
 docs = [komoran.nouns(sentence[1]) for sentence in review_data]
 print('3. word2Vec 모델 학습')
 # 하이퍼 파라미터들. 머신러닝 or 딥러닝에서 사용자가 지정해야 하는 값들
+#scipy를 1.13이 아니라 1.12로 바꿔야 하고
+#size 대신 vector_size로 바꿔야 함
+#install 한 번에 많이 누르지 말아야 함;;
 model = Word2Vec(sentences=docs, vector_size=200, window=4, min_count=2, sg=1)
 print('4. 학습된 모델 저장')
 model.save('nvmc.model')
